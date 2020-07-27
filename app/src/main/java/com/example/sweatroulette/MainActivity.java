56c63312349b2f660d6454f3d47b6a2ee2336896
Marti Kandallu <marti.kandallu@gmail.com>
2020-07-27 15:51:03 -0700

    Started onCheckboxClicked logic

--- app/src/main/java/com/example/sweatroulette/MainActivity.java
+++ app/src/main/java/com/example/sweatroulette/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.sweatroulette;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private List<String> exerciseList;
+    private int userReps = 0;
+    private CheckBox pushUps;
+    private TextView exerciseView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        exerciseList = new ArrayList<>();
+
         setContentView(R.layout.activity_main);
+
+        pushUps = findViewById(R.id.pushups);
+
+        exerciseView = findViewById(R.id.exerciseView);
+
     }
+
+    public void onCheckboxClicked(View view){
+
+        exerciseList.add( view.toString());
+        
+        exerciseView.setText("" + exerciseList.toString());
+
+
+    }
+
+
 }
