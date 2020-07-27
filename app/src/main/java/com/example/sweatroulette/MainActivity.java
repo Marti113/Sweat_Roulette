845fa0203a57cf27ef958329c596664b88f88f06
Marti Kandallu <marti.kandallu@gmail.com>
2020-07-27 16:55:47 -0700

    Started begin workout button, userReps needs fixing

--- app/src/main/java/com/example/sweatroulette/MainActivity.java
+++ app/src/main/java/com/example/sweatroulette/MainActivity.java
@@ -4,45 +4,58 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
+
 import java.util.Set;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Set<String> exerciseSet;
-    private List<String> exerciseList;
-    private int userReps = 0;
+    private int userReps;
     private CheckBox pushUps;
     private TextView exerciseView;
+    private Button startWorkout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        exerciseList = new ArrayList<>();
-        exerciseSet = new HashSet<>();
-
         setContentView(R.layout.activity_main);
 
+        exerciseSet = new HashSet<>();
         pushUps = findViewById(R.id.pushups);
-
+        //need to get a value from the user (Reps) and turn into an int
+        //userReps = Integer.parseInt(findViewById(R.id.numberSigned).toString());
         exerciseView = findViewById(R.id.exerciseView);
-
+        //start workout button and click listener
+        startWorkout = findViewById(R.id.workout);
+        startWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                workout();
+            }
+        });
     }
 
     public void onCheckboxClicked(View view){
 
-        //exerciseList.add( view.toString();
         String exercise = ((CheckBox) view).getText().toString();
         exerciseSet.add(exercise);
         exerciseView.setText("" + exerciseSet);
 
-
     }
 
 
+    public void workout(){
+        //code that loops through the set with a random number 0-userReps
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }
