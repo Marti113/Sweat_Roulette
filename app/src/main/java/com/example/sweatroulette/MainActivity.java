c6b8218a5983ab00defc1a53bc4a29527e4ea1ca
Marti Kandallu <marti.kandallu@gmail.com>
2020-07-27 16:21:13 -0700

    Finished onCheckboxClicked logic, values put in a Set

--- app/src/main/java/com/example/sweatroulette/MainActivity.java
+++ app/src/main/java/com/example/sweatroulette/MainActivity.java
@@ -8,10 +8,13 @@ import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity  {
 
+    private Set<String> exerciseSet;
     private List<String> exerciseList;
     private int userReps = 0;
     private CheckBox pushUps;
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         exerciseList = new ArrayList<>();
+        exerciseSet = new HashSet<>();
 
         setContentView(R.layout.activity_main);
 
@@ -32,9 +36,10 @@ public class MainActivity extends AppCompatActivity  {
 
     public void onCheckboxClicked(View view){
 
-        exerciseList.add( view.toString());
-        
-        exerciseView.setText("" + exerciseList.toString());
+        //exerciseList.add( view.toString();
+        String exercise = ((CheckBox) view).getText().toString();
+        exerciseSet.add(exercise);
+        exerciseView.setText("" + exerciseSet);
 
 
     }
